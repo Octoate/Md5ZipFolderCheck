b5afd9bae8bc3b5d69e7d6f77bb0113d28a8e4bc
Tim Riemann <triemann@octoate.de>
2013-11-11 19:56:10 +0100

    error detection for form data

--- MD5ZipCheck/MainForm.cs
+++ MD5ZipCheck/MainForm.cs
@@ -38,8 +38,34 @@ namespace MD5ZipCheck
 
         private void m_CheckFileButton_Click(object sender, EventArgs e)
         {
-            var compareForm = new CompareForm(m_MD5HashTextbox.Text, m_ZipFilePathTextbox.Text, m_CompareDirectoryTextbox.Text);
-            compareForm.Show();
+            if (CheckParameters())
+            {
+                var compareForm = new CompareForm(m_MD5HashTextbox.Text, m_ZipFilePathTextbox.Text, m_CompareDirectoryTextbox.Text);
+                compareForm.Show();
+            }
+        }
+
+        private bool CheckParameters()
+        {
+            if (!(m_MD5HashTextbox.Text.Replace(" ", "").Replace("-", "").Length == 32))
+            {
+                MessageBox.Show("Invalid MD5 hash", "Invalid MD5 hash (32 character hex string expected).", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            if (!File.Exists(m_ZipFilePathTextbox.Text))
+            {
+                MessageBox.Show("ZIP file not found", string.Format("ZIP file not found: '{0}'", m_ZipFilePathTextbox.Text), MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            if (!Directory.Exists(m_CompareDirectoryTextbox.Text))
+            {
+                MessageBox.Show("Directory not found", string.Format("Directory not found: '{0}'", m_CompareDirectoryTextbox.Text), MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            return true;
         }
     }
 }
