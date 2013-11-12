9445c40d6c634916a88ee139b20976ef680689e5
Tim Riemann <triemann@octoate.de>
2013-11-12 22:17:51 +0100

    various tests for compare class

--- MD5ZipFolderCheckTests/Md5ComparisonTests.cs
+++ MD5ZipFolderCheckTests/Md5ComparisonTests.cs
@@ -7,85 +7,188 @@ using MD5ZipCheck;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO.Compression;
 using System.IO;
+using System.Security.Cryptography;
 namespace MD5ZipCheck.Tests
 {
     [TestClass()]
     public class Md5ComparisonTests
     {
-        private string TempPath { get; set; }
-        private string ZipFileName { get; set; }
-        private string SubdirectoryName { get; set; }
+        private string TempPath { get { return Path.GetTempPath(); } }
+        private string ZipFileName { get { return "test.zip"; } }
+        private string ZipFilePath { get { return Path.Combine(TempPath, ZipFileName); } }
+        private string ZipMd5Hash { get; set; }
+        private string SubdirectoryName { get { return "test"; } }
+        private string SubdirectoryPath { get { return Path.Combine(TempPath, SubdirectoryName); } }
+        private string SubSubdirectoryPath { get { return Path.Combine(SubdirectoryPath, "subdir"); } }
 
         [TestInitialize]
         public void InitializeTest()
         {
-            //create a ZIP file in the tmp folder
-            TempPath = Path.GetTempPath();
-            ZipFileName = "test.zip";
-            SubdirectoryName = "test";
-
             //create test directory and test files
-            var subdirectoryPath = Path.Combine(TempPath, SubdirectoryName);
-            Directory.CreateDirectory(subdirectoryPath);
+            Directory.CreateDirectory(SubdirectoryPath);
 
             var random = new Random(System.Environment.TickCount);
             var buffer = new byte[4096];
             for (int i = 0; i < 5; i++)
             {
-                var file = File.Create(Path.Combine(subdirectoryPath, "test" + i + ".tst"));
+                var file = File.Create(Path.Combine(SubdirectoryPath, "test" + i + ".tst"));
                 random.NextBytes(buffer);
                 file.Write(buffer, 0, buffer.Length);
                 file.Close();
             }
 
-            Directory.CreateDirectory(Path.Combine(subdirectoryPath, "subdir"));
+            Directory.CreateDirectory(SubSubdirectoryPath);
             for (int i = 0; i < 5; i++)
             {
-                var file = File.Create(Path.Combine(subdirectoryPath, "subdir", "test" + i + ".tst"));
+                var file = File.Create(Path.Combine(SubSubdirectoryPath, "test" + i + ".tst"));
                 random.NextBytes(buffer);
                 file.Write(buffer, 0, buffer.Length);
                 file.Close();
             }
 
-            //create an example ZIP file
-            ZipFile.CreateFromDirectory(subdirectoryPath, Path.Combine(TempPath, ZipFileName));
+            //create an example ZIP file and compute the correct MD5 hash
+            ZipFile.CreateFromDirectory(SubdirectoryPath, Path.Combine(TempPath, ZipFileName));
+            ZipMd5Hash = GetMd5HashFromFile(ZipFilePath);
         }
 
         [TestCleanup]
         public void CleanUpTest()
         {
-            Directory.Delete(Path.Combine(TempPath, SubdirectoryName), true);
-            File.Delete(Path.Combine(TempPath, ZipFileName));
+            Directory.Delete(SubdirectoryPath, true);
+            File.Delete(ZipFilePath);
         }
-        
-        [TestMethod()]
-        public void Md5ComparisonTest()
+
+        private string GetMd5HashFromFile(string file)
         {
-            Assert.Fail();
+            var fileStream = File.OpenRead(file);
+            var md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(fileStream);
+            fileStream.Close();
+
+            return BitConverter.ToString(hash).Replace("-", "");
         }
 
         [TestMethod()]
-        public void Md5ComparisonTest1()
+        public void Md5ComparisonTestOk()
         {
-            Assert.Fail();
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.Ok, md5Comparison.Compare());
+
+            md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath, Console.Out);
+            Assert.AreEqual(CompareResult.Ok, md5Comparison.Compare());
+
+            var cliArguments = new CommandLineArguments();
+            cliArguments.Md5Hash = ZipMd5Hash;
+            cliArguments.ZipFilePath = ZipFilePath;
+            cliArguments.CompareFolder = SubdirectoryPath;
+
+            md5Comparison = new Md5Comparison(cliArguments);
+            Assert.AreEqual(CompareResult.Ok, md5Comparison.Compare());
+
+            md5Comparison = new Md5Comparison(cliArguments, Console.Out);
+            Assert.AreEqual(CompareResult.Ok, md5Comparison.Compare());
         }
 
         [TestMethod()]
-        public void Md5ComparisonTest2()
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidMd5Hash()
         {
-            Assert.Fail();
+            var md5Comparison = new Md5Comparison(ZipMd5Hash + "A", ZipFilePath, SubdirectoryPath);
         }
 
         [TestMethod()]
-        public void Md5ComparisonTest3()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Md5HashNull()
         {
-            Assert.Fail();
+            var md5Comparison = new Md5Comparison(null, ZipFilePath, SubdirectoryPath, null);
         }
 
         [TestMethod()]
-        public void CompareTest()
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void InvalidZipFile()
         {
-            Assert.Fail();
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath + "A", SubdirectoryPath);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipFilePathNull()
+        {
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, null, SubdirectoryPath, null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void InvalidCompareFolder()
+        {
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath + "A");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DirectoryNull()
+        {
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TextWriterNull()
+        {
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath, null);
+        }
+
+        [TestMethod()]
+        public void WrongMd5Hash()
+        {
+            var md5Comparison = new Md5Comparison(ZipMd5Hash.Remove(ZipMd5Hash.Length - 1).Insert(0, "X"), ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.InvalidZipHash, md5Comparison.Compare());
+        }
+
+        [TestMethod()]
+        public void WrongHashForFileInDirectory()
+        {
+            var file = File.OpenWrite(Path.Combine(SubdirectoryPath, "test1.tst"));
+            file.WriteByte(0xFF);
+            file.Close();
+
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.InvalidFileHash, md5Comparison.Compare());
+        }
+
+        [TestMethod()]
+        public void WrongHashForFileInSubDirectory()
+        {
+            var file = File.OpenWrite(Path.Combine(SubSubdirectoryPath, "test1.tst"));
+            file.WriteByte(0xFF);
+            file.Close();
+
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.InvalidFileHash, md5Comparison.Compare());
+        }
+
+        [TestMethod()]
+        public void CorruptZipFile()
+        {
+            var file = File.OpenWrite(ZipFilePath);
+            file.WriteByte(0xFF);
+            file.Close();
+
+            var md5Comparison = new Md5Comparison(ZipMd5Hash, ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.InvalidZipHash, md5Comparison.Compare());
+        }
+
+        [TestMethod()]
+        public void CorruptZipFileWithCorrectHash()
+        {
+            var file = File.OpenWrite(ZipFilePath);
+            file.WriteByte(0xFF);
+            file.Close();
+
+            var md5Hash = GetMd5HashFromFile(ZipFilePath);
+
+            var md5Comparison = new Md5Comparison(md5Hash, ZipFilePath, SubdirectoryPath);
+            Assert.AreEqual(CompareResult.CorruptZipFile, md5Comparison.Compare());
         }
     }
 }
